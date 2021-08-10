9ca5e0395e01c0663083f2e6f90d5bbe006dce66
chiton0 <85007583+chiton0@users.noreply.github.com>
2021-08-10 23:43:53 +0300

    feat: implement the functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+
+ return array === undefined || array.length === 0 ? 0 : array.reduce((min, current) => min > current ? current : min, Number.MAX_VALUE);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array === undefined || array.length === 0 ? 0 : array.reduce((max, current) => max < current ? current : max, Number.MIN_VALUE);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let sum = array.reduce((sum, current) => sum + current, 0);
+  return sum / array.length;
 }
